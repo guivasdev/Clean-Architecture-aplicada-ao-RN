249b0533f4bda34e94311c1f6bb4b8db3333137a
Guilherme V.Baldesin <guivasbaldesin@gmail.com>
2026-03-13 19:31:00 -0300

    estrutura base finalizada

---
+++ app/features/exchange/components/ExchangeItem.tsx
@@ -0,0 +1,36 @@
+import { StyleProp, TextStyle, View, ViewStyle } from "react-native"
+import { useAppTheme } from "@/theme/context"
+import type { ThemedStyle } from "@/theme/types"
+import { Text } from "@/components/Text"
+
+export interface ExchangeItemProps {
+  /**
+   * An optional style override useful for padding & margin.
+   */
+  style?: StyleProp<ViewStyle>
+}
+
+/**
+ * Describe your component here
+ */
+export const ExchangeItem = (props: ExchangeItemProps) => {
+  const { style } = props
+  const $styles = [$container, style]
+  const { themed } = useAppTheme();
+
+  return (
+    <View style={$styles}>
+      <Text style={themed($text)}>Hello</Text>
+    </View>
+  )
+}
+
+const $container: ViewStyle = {
+  justifyContent: "center",
+}
+
+const $text: ThemedStyle<TextStyle> = ({ colors, typography }) => ({
+  fontFamily: typography.primary.normal,
+  fontSize: 14,
+  color: colors.palette.primary500,
+})
